4376c6fe14275dc16f2cc978e7d3ff7a92c9067b
Niraj Srivastava <54449124+NirajSrivastava18@users.noreply.github.com>
2020-02-13 13:06:55 +0530

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
-// TODO: In day 2 of workshop
+// TODO: In day 2 of workshopx
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+ @override
+ Widget build(BuildContext context){
+   return MaterialApp(
+   home: Scaffold(
+     appBar:AppBar
+     (title: Text("hello world"),),
+     body:Column(
+       children: <Widget>[
+         Text('hello world')
+
+       ]//<widget>{}
+     ),//column
+
+   )//Scaffold
+   );//materialfold
+  
+ }
+}
